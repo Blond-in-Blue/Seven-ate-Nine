ec4bf14ae8d239f8eb10919bb7312a66482652ae
stimpak-addict <hholder@me.com>
2015-05-16 22:15:38 -0500

    Initial Commit

---
+++ Seven ate Nine/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Seven ate Nine
+//
+//  Created by Hunter Holder on 5/16/15.
+//  Copyright (c) 2015 Hunter Holder. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
